a54efbe6e6d41cffe3cdb210debaa6ff4cb56bf3
stefania-rossi <stefania.rossi@alessandrinimainardi.edu.it>
2025-10-20 13:37:59 +0200

    modificato allowed host

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_y+vp3tq%0kci)#nh@&ct+l-#_8$$nyl)j$7b7z9z3uk)z=mld
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
