2c5a981d6b6f8f7b300a1c503e5fc1c2bedc8c4d
stefania-rossi <stefania.rossi@alessandrinimainardi.edu.it>
2026-01-19 13:54:59 +0100

    inizio passo 18

--- news/models.py
+++ news/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Giornalista(models.Model):
     nome = models.CharField(max_length=20)
     cognome = models.CharField(max_length=20)
+    anno_di_nascita = models.DateField(blank=True)
 
     def __str__(self):
         return self.nome + " " + self.cognome
