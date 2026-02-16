4f9368e0f47936dfa9e9fa3b30a3013803a6ba75
stefania-rossi <stefania.rossi@alessandrinimainardi.edu.it>
2026-02-16 11:42:41 +0100

    sistemazioni

--- prova_pratica_0/urls.py
+++ prova_pratica_0/urls.py
@@ -3,7 +3,7 @@ from prova_pratica_0.views import index_prova, somma, media
 
 app_name="prova_pratica_0"
 urlpatterns=[
-    path('index_prova',index_prova,name='index_prova'),
+    path('',index_prova,name='index_prova'),
     path('somma',somma,name='somma'),
     path('media',media,name='media')
 ]
--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -6,6 +6,6 @@ urlpatterns=[
     path('es_if', es_if, name='es_if'),
     path('if_else_elif', if_else_elif, name = 'if_else_elif'),
     path('es_for', es_for, name = 'es_for'),
-    path('index_2', index_2, name='index_2')
+    path('', index_2, name='index_2')
 ]
 
